59fe858f582275080b9b8d87c22c00de2b22dd27
Kayce Heap <kayce.heap@gmail.com>
2022-02-03 18:26:18 -0700

    added inquirer and added the questions

---
+++ index.js
@@ -0,0 +1,55 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?"
+},
+{
+    type: "input",
+    name: "description",
+    message: "What is the description of your project?"
+},
+{
+    type: "input",
+    name: "toc",
+    message: "What is the table of contents?"
+},
+{
+    type: "input",
+    name: "installation",
+    message: "What are the installation instructions?"
+},
+{
+    type: "input",
+    name: "usage",
+    message: "What is the usage information?"
+},
+{
+    type: "input",
+    name: "contribution",
+    message: "What are the contribution guidelines?"
+},
+{
+    type: "input",
+    name: "test",
+    message: "What are the test insructions?"
+},
+{
+    type: "input",
+    name: "questions",
+    message: "What are the questions?"
+}];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions);
+}
+
+// Function call to initialize app
+init();
